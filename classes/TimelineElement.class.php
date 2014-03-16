143783ed89681a48f6813d8866f849aec219e024
Tommy Crush <TommyCrush@gmail.com>
2014-03-16 10:50:14 -0400

    Base class for timeline elements created.

---
+++ classes/TimelineElement.class.php
@@ -0,0 +1,71 @@
+<?php
+
+
+abstract class TimelineElement {
+	
+	// child classes should fil
+	protected $title, $numReblogs, $numLikes, $timestamp, $author;
+
+	protected $icon_name = '';
+
+	// override this will timeline-body content
+	abstract protected function renderContentBody();
+
+	public function renderHTML() {
+		$html = '';
+		$html .= $this->renderBadge();
+		$html .= '<div class="timeline-panel">';
+		$html .= $this->renderTitle();
+		$html .= '<div class="timeline-body">';
+		$html .= $this->renderContentBody();
+		$html .= $this->renderNotes();
+		$html .= '</div></div><!-- /.timeline-panel -->';
+
+		return $html;
+	}
+
+	private function renderBadge() {
+		return 
+			'<div class="timeline-badge warning">
+				<i class="glyphicon glyphicon-'.$this->icon_name.'"></i>
+			</div>';
+	}
+
+	private function renderTitle() {
+		if ($this->title) {
+			return 
+				'<div class="timeline-heading">
+					<h4 class="timeline-title">'.$this->title.'</h4>
+				</div>';
+		}
+		return '';
+	}
+
+	private function renderNotes() {
+		$likes_noun = " likes";
+		if($this->likes == 1){
+			$likes_noun = " like";
+		}
+
+		$reblogs_noun = " reblogs";
+		if($this->reblogs == 1){
+			$reblogs_noun = " reblog";
+		}
+
+		return '
+		<p class="notes">
+          <small class="text-muted">
+           <i class="glyphicon glyphicon-thumbs-up"></i> '.$this->likes.$likes_noun.'
+          </small>
+          <small class="text-muted">
+           <i class="glyphicon glyphicon-fullscreen"></i> '.$this->reblogs.$reblogs_noun.'
+          </small>
+          <small class="text-muted">
+           <i class="glyphicon glyphicon-time"></i> 11 hours ago 
+           by <a href="#">'.$this->author.'</a>
+          </small>
+        </p>';
+	}
+
+
+}
